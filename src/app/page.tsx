4b8c2a254b259fd12ba9a3e15901ac1f572a371c
Chrystalio <chrystalio.k@outlook.com>
2024-10-15 23:27:59 +0700

    Update project section copywriting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,12 +113,10 @@ export default function Page() {
                   My Projects
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
+                  Explore my work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
+                   I've had the pleasure of working on a variety of interesting projects. take a look at some that I'm proud of below.
                 </p>
               </div>
             </div>
